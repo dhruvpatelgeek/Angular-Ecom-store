c4c79144165a24f52f97f6f9c5010c055ccdeb75
dhruvpatelgeek <dhruvpatel1999@outlook.in>
2021-01-01 20:39:57 -0800

    final

--- frontend/frontend/src/app/service/freeapi.service.ts
+++ frontend/frontend/src/app/service/freeapi.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http'
 import { Addtocart } from '../classes/addtocart';
 @Injectable()//declaring a class of type injectable
 export class freeapiservice{
-    url="http://169.55.163.41:3000";//DRY principle
+    url="https://bribchat.com:44369";//DRY principle
     //now we will inject the httpt client using thr constuctor
     constructor(private httpclient: HttpClient){};
 
